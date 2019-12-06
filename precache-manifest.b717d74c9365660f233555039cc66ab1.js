16eb0cc40dfa8a8643344efdf6d7d2c99e8c0cc4
DPDP93 <47352728+DPDP93@users.noreply.github.com>
2019-12-06 11:20:55 +0100

    first commit

---
+++ precache-manifest.b717d74c9365660f233555039cc66ab1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cc5b24da454d0c015cb3a3f6bfbe78e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c01f22dc02a97571391",
+    "url": "/static/js/2.68501642.chunk.js"
+  },
+  {
+    "revision": "e7138eefc09780202021",
+    "url": "/static/js/main.2ce7a79d.chunk.js"
+  },
+  {
+    "revision": "014f83a7918d6be96794",
+    "url": "/static/js/runtime-main.6e049e34.js"
+  }
+]);
