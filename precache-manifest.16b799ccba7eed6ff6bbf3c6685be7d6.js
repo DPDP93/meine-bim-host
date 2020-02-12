7d757b6fc6df190688a0eef53b9b5a174b3a0896
dprvulov <dejan.prv@gmail.com>
2020-02-12 16:38:03 +0100

    improvement perf

--- precache-manifest.16b799ccba7eed6ff6bbf3c6685be7d6.js
+++ precache-manifest.16b799ccba7eed6ff6bbf3c6685be7d6.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9f0e31888946f57c3df864cb1295ed7d",
+    "revision": "8d0b1aaf66c8c2dda6875dc62db5ba8a",
     "url": "/index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.2d358782.chunk.js.LICENSE"
   },
   {
-    "revision": "1a8e2c9f10d42109582c",
-    "url": "/static/js/main.bd63505a.chunk.js"
+    "revision": "2c859adb55752276a552",
+    "url": "/static/js/main.1ee5f59e.chunk.js"
   },
   {
     "revision": "e38e51475aa13c2b57ee",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.23f4518e0bee75433554cdf6aef3bb35.js"
+  "/precache-manifest.16b799ccba7eed6ff6bbf3c6685be7d6.js"
 );
 
 self.addEventListener('message', (event) => {
